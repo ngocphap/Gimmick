d0cb2846e6f93696f982258245759e28cd670ba3
ngocphap <ngocphap5@gmail.com>
2021-06-20 12:55:13 +0700

    first commit

---
+++ Assets/Scripts/AssetPalette.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssetPalette : MonoBehaviour
+{
+    // enemy prefabs
+    public GameObject[] enemyPrefabs = new GameObject[4];
+    public enum EnemyList
+    {
+        BigEye,
+        KillerBomb,
+        Mambu,
+        Pepe
+    };
+    public EnemyList enemyList;
+
+    // item prefabs
+    public GameObject[] itemPrefabs = new GameObject[5];
+    public enum ItemList
+    {
+        BonusBall,
+        //ExtraLife,
+        LifeEnergyBig,
+        LifeEnergySmall,
+        WeaponEnergyBig,
+        WeaponEnergySmall,
+        //MagnetBeam,
+       // WeaponPart,
+       // Yashichi
+    };
+    public ItemList itemList;
+}
